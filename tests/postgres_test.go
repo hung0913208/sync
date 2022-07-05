19bbb5dcb75aa71fcd559135a65100ae14434a06
Hung Nguyen Xuan Pham <sonpham@monsterpixel.us>
2022-07-05 19:17:32 +0700

    [ADO-671] Make everything works

--- tests/postgres_test.go
+++ tests/postgres_test.go
@@ -1,8 +1,12 @@
 package main
 
 import (
+    "path/filepath"
+    "io/ioutil"
     "testing"
+    "time"
     "fmt"
+    "os"
 
     "alpaca.vn/libra/devops/sync/lib/notify"
 
@@ -25,7 +29,7 @@ type Test struct {
     Name    string
 }
 
-func (suite *ExampleTestSuite) SetupTest() {
+func (suite *PostgresTestSuite) SetupTest() {
     dsn := "host=postgres user=postgres port=5432 sslmode=disable"
 
     db, err := gorm.Open(postgres.Open(dsn), &gorm.Config{}) 
@@ -43,20 +47,25 @@ func (suite *ExampleTestSuite) SetupTest() {
         panic(err)
     }
 
-    err := filepath.Walk("fixture", func(path string, f os.FileInfo, err error) error {
+    err = filepath.Walk("fixture", func(path string, f os.FileInfo, err error) error {
         if filepath.Ext(path) == "sql" {
             query, err := ioutil.ReadFile(path)
             if err != nil {
                 return err
             }
 
-            db.Exec(query)
+            db.Exec(string(query))
         }
 
         return nil
     })
 
-    suite.notifier = notify.NewPostgresNotifier(dsn) 
+    notifier, err := notify.NewPostgresNotifier(dsn) 
+    if err != nil {
+        panic(err)
+    }
+
+    suite.notifier = notifier
     suite.db = db
 }
 
@@ -67,14 +76,15 @@ func TestPostgres(t *testing.T) {
 func (suite *PostgresTestSuite) TestCatchingInsertRequest() {
     cnt := 0
 
-    result := suite.db.Create(&Test{Id: 1, Name: "test"} 
+    result := suite.db.Create(&Test{Id: 1, Name: "test"})
     assert.Nil(suite.T(), result.Error)
 
-    suite.notifier.Notify(func(msg string)) error {
+    suite.notifier.Notify(func(msg string) error {
         cnt += 1
         fmt.Println(msg)
         return nil
-    }
+    },
+    time.Second)
 
     assert.Equal(suite.T(), cnt, 1)
 }
